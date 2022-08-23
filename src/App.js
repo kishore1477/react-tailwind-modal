901f84489ff1c50206b0f683b5ab05802a9b6d6f
kishore <kishorejaipal477@gmail.com>
2022-08-22 19:31:41 -0700

    apply  turnary operater to handle modal response

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Modal from './Modal';
+import react ,{useState}from 'react'
 function App() {
+  const [choice, setChoice] = useState(false);
+  const [show, setShow] = useState(false);
+  const click = () => {
+    // alert("Clicked")
+    // logic for toggle mode
+//     if(show){
+//       setShow(false)
+
+//     }else if(!show){
+// setShow(true)
+//     }
+
+setShow(true)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+     { choice ? <div className='mt-12 flex justify-center '>
+<h5 className='w-1/3  text-center   bg-yellow-600 rounded-lg py-1 '>Welcome buddy</h5>
+      </div>: <div className='flex justify-center'>
+
+<div className='bg-slate-900  text-center cursor-pointer rounded  w-1/3 text-white ' onClick={click}>Click Me</div>
+</div>}
+      { show && <Modal setChoice={setChoice} setShow={setShow} />}
+    </>
   );
 }
 
